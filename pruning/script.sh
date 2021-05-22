616c5932b3e8016a77c0e195bc9d238142cb9050
Ali Asgari <altostratous@gmail.com>
2021-05-22 07:58:18 -0700

    Add slurm job

---
+++ pruning/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --time=1:00:00
+#SBATCH --gres=gpu:p100:1
+#SBATCH --mem=32G
+#SBATCH --array 0-189
+
+export PYTHONPATH=/home/aasgarik/projects/def-karthikp/aasgarik/reasilience
+source /home/aasgarik/projects/def-karthikp/aasgarik/dnnfault/venv/bin/activate
+pythor actions.py
