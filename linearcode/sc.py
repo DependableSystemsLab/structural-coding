43e3d53faa71fe46c4ab8204046ded0a69e88bbf
Ali Asgari <altostratous@gmail.com>
2021-09-24 12:35:58 -0700

    Implement structural coding on pytorch

--- linearcode/sc.py
+++ linearcode/sc.py
@@ -1,49 +1,78 @@
 import random
 from itertools import combinations
+from typing import Optional
 
 import torch
-from torch import Tensor, FloatTensor
+from torch import Tensor, LongTensor
+
+
+class ErasureCode:
+    def __init__(self, k) -> None:
+        self.k = k
+
+    @staticmethod
+    def checksum(tensor: Tensor, dim: int = 0) -> Tensor:
+        return torch.sum(tensor, dim=tuple(d for d in range(len(tensor.shape)) if d != dim))
+
+    def erasure(self, tensor: Tensor, checksum: Tensor, dim: int = 0) -> Tensor:
+        return torch.sort(torch.topk(torch.abs(self.checksum(tensor, dim) - checksum), self.k).indices).values
 
 
 class StructuralCode:
 
-    def __init__(self, n, k, p, threshold=0) -> None:
+    def __init__(self, n, k, threshold=0) -> None:
         self.n = n
         self.k = k
-        self.p = p
+        self._weights = None
         self.threshold = threshold
 
-    def code(self, tensor: Tensor, dim: int = 0, weight_stop=None, weights=None) -> Tensor:
+    def _code(self, tensor: Tensor, dim: int = 0, weight_stop=None, weights=None) -> Tensor:
         if weights is None:
             weights = self._generate_redundant_weights()
         if weight_stop is None:
-            weight_stop = self.p + self.k
-        return torch.cat((tensor, self.out_transform(torch.matmul(self.in_transpose(tensor, dim), weights[:, :weight_stop]), dim)), dim)
+            weight_stop = self.k
+        return self.out_transform(torch.matmul(self.in_transpose(tensor, dim), weights[:, :weight_stop]), dim)
 
-    def decode(self, tensor: Tensor, dim: int = 0) -> Tensor:
+    def code(self, tensor: Tensor, dim: int = 0, weight_stop=None, weights=None) -> Tensor:
+        return torch.cat((tensor, self._code(tensor, dim, weight_stop, weights)), dim)
+
+    def decode(self, tensor: Tensor, dim: int = 0, erasure: Tensor = None) -> Optional[Tensor]:
         tensor = self.in_transpose(tensor, dim, 0)
         systematic = tensor[: self.n]
-        bloom = tensor[self.n: self.n + self.k]
-        checksum = torch.sum(bloom[:2] - self.checksum(systematic)[self.n:])
+        checksum = torch.sum(tensor[self.n] - self.checksum(systematic))
         if abs(checksum) <= self.threshold:
             return self.out_transform(systematic, dim, 0)
-        linear = tensor[self.n:]
-        bloom = self.brute_decode(linear)
-        faulty_bloom = self.code(systematic)[self.n: self.n + self.k]
-        bloom_pattern = torch.abs(torch.sum(bloom - faulty_bloom, tuple(range(1, len(bloom.shape))))) > self.threshold
-        print(bloom_pattern)
+        if erasure is None:
+            return None
+        erasure = torch.sort(erasure).values
+        healthy_indices = LongTensor(list(set(range(tensor.shape[0])) - set(map(int, erasure))))
+        systematic_healthy_indices = LongTensor([i for i in set(range(tensor.shape[0])) - set(map(int, erasure)) if i < self.n])
+        weights = torch.cat((torch.eye(self.n), self._generate_redundant_weights()), 1)
 
-    def patch(self, tensor: Tensor, dim: int = 0) -> Tensor:
-        return None
+        # keep healthy redundant vectors
+        weights = weights[:, healthy_indices]
+        redundant_part = tensor[healthy_indices]
+
+        print(weights.shape)
+        patch = torch.pinverse(weights)
+
+        reconstructed_erasure = self.in_transpose(torch.matmul(self.out_transform(redundant_part, 0), patch))
+
+        reconstructed_erasure[systematic_healthy_indices] = tensor[systematic_healthy_indices]
+
+        return self.out_transform(reconstructed_erasure, 0, dim)
+
 
     def _generate_redundant_weights(self) -> Tensor:
-        result = torch.zeros((self.n, self.k))
-        bloom_keys = self._generate_bloom_keys()
-        for i in range(self.n):
-            for j in bloom_keys[i]:
-                result[i][j] += 1
-        brute_weights = self._generate_brute_weights()
-        result = torch.cat((result, torch.matmul(result, brute_weights)), 1)
+        if self._weights is None:
+            self._weights = torch.rand((self.n, self.k))
+        return torch.clone(self._weights)
+        result = torch.ones((self.n, self.k))
+        counter = 0
+        for j in range(self.n):
+            for i in range(self.k):
+                result[j][i] += counter
+                counter += 1
         return result
 
     def _generate_bloom_keys(self):
@@ -59,38 +88,34 @@ class StructuralCode:
             destination = len(tensor.shape) - 1
         return torch.transpose(tensor, destination, dim)
 
-    def checksum(self, systematic: Tensor, dim=0):
-        return self.code(systematic, dim, 2)
+    def checksum(self, systematic: Tensor, dim=0) -> Tensor:
+        return self._code(systematic, dim, 1)[0]
 
-    def brute_decode(self, linear: Tensor) -> Tensor:
-        brute_weights = self._generate_brute_weights()
-        checksum = torch.sum(torch.matmul(self.in_transpose(linear[:self.k]), brute_weights[:, 0]) - linear[self.k])
-        if checksum <= self.threshold:
-            return linear[:self.k]
-        assert False
-        weights = torch.cat((torch.eye(self.k), brute_weights))
-        for in_hold in combinations(range(self.k + self.p), self.k + 1):
-            hold = linear[in_hold]
-            hold_weights = weights[in_hold]
-            target = hold[self.k]
-            target_weights = hold_weights[self.k]
+    def extract_systematic(self, tensor: Tensor, dim: int = 0) -> Tensor:
+        tensor = self.in_transpose(tensor, dim, 0)
+        systematic = tensor[: self.n]
+        return self.out_transform(systematic, dim, 0)
 
 
-    def _generate_brute_weights(self) -> Tensor:
-        result = torch.ones((self.k, self.p))
-        counter = 0
-        for i in range(1, self.p):
-            for j in range(self.k):
-                result[j][i] += counter
-                counter += 1
-        return result
-
-
-coding = StructuralCode(64, 8, 4)
-
-plain = torch.ones((2, 64, 2))
-codeword = coding.code(plain, 1)
-for _ in range(3):
-    corruption_index = random.randrange(0, 64)
-    codeword[:, corruption_index, :] = random.randrange(0, 64)
-print(plain - coding.decode(codeword, 1))
+k = 64
+coding_dim = 1
+block_size = 64
+plain = torch.rand((2, block_size, 2))
+coding = StructuralCode(block_size, k)
+erasure_coding = ErasureCode(k)
+codeword = coding.code(plain, coding_dim)
+erasure_checksum = erasure_coding.checksum(plain, coding_dim)
+corrupted_index = []
+for _ in range(k):
+    corruption_index = random.randrange(0, block_size + k)
+    codeword[:, corruption_index, :] = random.randrange(0, block_size)
+    corrupted_index.append(corruption_index)
+# assert coding.decode(codeword, 1) is None
+# erasure = erasure_coding.erasure(coding.extract_systematic(codeword, coding_dim), erasure_checksum, coding_dim)
+corrupted_index = sorted(corrupted_index)
+erasure = LongTensor(corrupted_index)
+decoded_plain = coding.decode(codeword, 1, erasure)
+print(corrupted_index)
+print(erasure)
+corrupted_index = [c for c in corrupted_index if c < block_size]
+print(plain - decoded_plain)
