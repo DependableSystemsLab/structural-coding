530e0ed740ed69716b5d9025a88691c3e57836e0
Ali Asgari <altostratous@gmail.com>
2021-10-29 12:24:24 -0700

    Add dave-2 comma driving

--- common/comma.py
+++ common/comma.py
@@ -11,6 +11,7 @@ import os
 import torch
 import torchvision
 import torch.nn as nn
+from torch import optim
 from torchvision import transforms
 
 
@@ -29,22 +30,26 @@ class Comma(nn.Module):
     * @param None.
     * @return None.
     """
-    def __init__(self):
+    def __init__(self, pretrained=True):
         super().__init__()
 
         self.elu = nn.ELU()
         self.dropout = nn.Dropout()
 
-        self.conv_0 = nn.Conv2d(3, 24, 5, stride=2)
-        self.conv_1 = nn.Conv2d(24, 36, kernel_size=5, stride=2)
-        self.conv_2 = nn.Conv2d(36, 48, kernel_size=5, stride=2) #384 kernels, size 3x3
-        self.conv_3 = nn.Conv2d(48, 64, kernel_size=3) # 384 kernels size 3x3
-        self.conv_4 = nn.Conv2d(64, 64, kernel_size=3) # 256 kernels, size 3x3
+        self.conv_0 = nn.Conv2d(3, 24, (5, 5), stride=(2, 2))
+        self.conv_1 = nn.Conv2d(24, 36, kernel_size=(5, 5), stride=(2, 2))
+        self.conv_2 = nn.Conv2d(36, 48, kernel_size=(5, 5), stride=(2, 2))  # 384 kernels, size 3x3
+        self.conv_3 = nn.Conv2d(48, 64, kernel_size=(3, 3))  # 384 kernels size 3x3
+        self.conv_4 = nn.Conv2d(64, 64, kernel_size=(3, 3))  # 256 kernels, size 3x3
 
-        self.fc0 = nn.Linear(1152, 100)
-        self.fc1 = nn.Linear(100,50)
+        # self.fc0 = nn.Linear(1152, 100)
+        self.fc0 = nn.Linear(1280, 100)
+        self.fc1 = nn.Linear(100, 50)
         self.fc2 = nn.Linear(50, 10)
         self.fc3 = nn.Linear(10, 1)
+        if pretrained:
+            self.load_state_dict(torch.load('comma/steering_angle.pth'))
+
     """ 
     * @brief Function to build the model.
     * @parma The image to train.
@@ -59,7 +64,7 @@ class Comma(nn.Module):
         x = self.elu(self.conv_4(x))
         x = self.dropout(x)
 
-        x = x.flatten()
+        x = x.flatten(start_dim=1)
         x = self.elu(self.fc0(x))
         x = self.elu(self.fc1(x))
         x = self.elu(self.fc2(x))
@@ -69,11 +74,69 @@ class Comma(nn.Module):
 
 
 if __name__ == '__main__':
-    transform = transforms.Compose([
-        transforms.Resize((320, 160)),
-        transforms.ToTensor()
-        ])
-    sample = torch.stack((transform(torchvision.datasets.folder.default_loader('../../Ranger/demo/sample_inputs/1000.jpg')),))
-    print(sample.shape)
-    model = Comma(pretrained=True)
-    print(model(sample))
+    if __name__ == "__main__":
+        from dask_generator import datagen
+        import argparse
+
+        # Parameters
+        parser = argparse.ArgumentParser(description='MiniBatch server')
+        parser.add_argument('--batch', dest='batch', type=int, default=256, help='Batch size')
+        parser.add_argument('--time', dest='time', type=int, default=1, help='Number of frames per sample')
+        parser.add_argument('--port', dest='port', type=int, default=5557, help='Port of the ZMQ server')
+        parser.add_argument('--buffer', dest='buffer', type=int, default=20,
+                            help='High-water mark. Increasing this increses buffer and memory usage.')
+        parser.add_argument('--prep', dest='prep', action='store_true', default=False,
+                            help='Use images preprocessed by vision model.')
+        parser.add_argument('--leads', dest='leads', action='store_true', default=False,
+                            help='Use x, y and speed radar lead info.')
+        parser.add_argument('--nogood', dest='nogood', action='store_true', default=False,
+                            help='Ignore `goods` filters.')
+        parser.add_argument('--validation', dest='validation', action='store_true', default=False,
+                            help='Serve validation dataset instead.')
+        args, more = parser.parse_known_args()
+
+        # 9 for training
+        train_path = [
+            '../data/comma/comma-dataset/camera/2016-01-30--11-24-51.h5',
+            '../data/comma/comma-dataset/camera/2016-01-30--13-46-00.h5',
+            '../data/comma/comma-dataset/camera/2016-01-31--19-19-25.h5',
+            '../data/comma/comma-dataset/camera/2016-02-02--10-16-58.h5',
+            '../data/comma/comma-dataset/camera/2016-02-08--14-56-28.h5',
+            '../data/comma/comma-dataset/camera/2016-02-11--21-32-47.h5',
+            '../data/comma/comma-dataset/camera/2016-03-29--10-50-20.h5',
+            '../data/comma/comma-dataset/camera/2016-04-21--14-48-08.h5',
+            '../data/comma/comma-dataset/camera/2016-05-12--22-20-00.h5',
+        ]
+
+        # 2 for validation
+        validation_path = [
+            '../data/comma/comma-dataset/camera/2016-06-02--21-39-29.h5',
+            '../data/comma/comma-dataset/camera/2016-06-08--11-46-01.h5'
+        ]
+
+        if args.validation:
+            datapath = validation_path
+        else:
+            datapath = train_path
+
+        gen = datagen(datapath, time_len=args.time, batch_size=args.batch, ignore_goods=args.nogood)
+        model = Comma(pretrained=False).to('cuda:0')
+        model.train()
+        optimizer = optim.Adam(model.parameters(), lr=1e-4)
+        criterion = nn.MSELoss()
+        for epoch in range(1000):  # runs for the number of eposchs set in the arguments
+            optimizer.zero_grad()
+            for x, y, _ in gen:
+                x = x.to('cuda:0')
+                y = y.to('cuda:0')
+                x = x/127.5 - 1
+                x = transforms.Resize(70)(x)
+                model_output = model(x)
+                loss = criterion(model_output, y)
+                print(loss)
+                loss.backward()
+                optimizer.step()
+            torch.save({
+                'epoch': epoch,
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict()}, 'comma_{}.pth'.format(epoch))
