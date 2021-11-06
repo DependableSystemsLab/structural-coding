6f3ef1215a0658daa91202252d6fc8ade881276e
Ali Asgari <altostratous@gmail.com>
2021-11-05 18:59:37 -0700

    Fix comma

--- common/dave.py
+++ common/dave.py
@@ -1,13 +1,18 @@
+import numpy
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 # https://github.com/HaojieYuu/pytorch_dave2/blob/master/model.py
-from torch import optim
+from torch import optim, FloatTensor
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 
 from datasets import DrivingDataset
 from settings import BATCH_SIZE
+
+
+def reverse_dims(param):
+    return param.permute(tuple(range(len(param.shape) - 1, -1, -1)))
 
 
 class Dave2(nn.Module):
@@ -19,14 +24,14 @@ class Dave2(nn.Module):
     def __init__(self, pretrained=True):
         super().__init__()
 
-        self.relu = nn.ReLU()
+        self.elu = nn.ELU()
         self.dropout = nn.Dropout()
 
-        self.conv_0 = nn.Conv2d(3, 24, (5, 5), stride=(2, 2))
-        self.conv_1 = nn.Conv2d(24, 36, kernel_size=(5, 5), stride=(2, 2))
-        self.conv_2 = nn.Conv2d(36, 48, kernel_size=(5, 5), stride=(2, 2))  # 384 kernels, size 3x3
-        self.conv_3 = nn.Conv2d(48, 64, kernel_size=(3, 3))  # 384 kernels size 3x3
-        self.conv_4 = nn.Conv2d(64, 64, kernel_size=(3, 3))  # 256 kernels, size 3x3
+        self.conv_0 = nn.Conv2d(3, 24, (5, 5), stride=(2, 2), padding='valid')
+        self.conv_1 = nn.Conv2d(24, 36, kernel_size=(5, 5), stride=(2, 2), padding='valid')
+        self.conv_2 = nn.Conv2d(36, 48, kernel_size=(5, 5), stride=(2, 2), padding='valid')  # 384 kernels, size 3x3
+        self.conv_3 = nn.Conv2d(48, 64, kernel_size=(3, 3), padding='valid')  # 384 kernels size 3x3
+        self.conv_4 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding='valid')  # 256 kernels, size 3x3
 
         self.fc0 = nn.Linear(1152, 100)
         # self.fc0 = nn.Linear(1280, 100)
@@ -34,7 +39,11 @@ class Dave2(nn.Module):
         self.fc2 = nn.Linear(50, 10)
         self.fc3 = nn.Linear(10, 1)
         if pretrained:
-            self.load_state_dict(torch.load('comma/steering_angle.pth'))
+            # for i, p in enumerate(self.parameters()):
+            #     with torch.no_grad():
+            #         p[:] = reverse_dims(FloatTensor(numpy.load('dave/{}.npz'.format(i))))
+            # torch.save(self.state_dict(), 'dave/steering_angle.pth')
+            self.load_state_dict(torch.load('dave/steering_angle.pth'))
 
     """ 
     * @brief Function to build the model.
@@ -42,19 +51,31 @@ class Dave2(nn.Module):
     * @return The trained prediction network.
     """
     def forward(self, x):
-        x = x / 127.5 - 1.0
-        x = self.relu(self.conv_0(x))
-        x = self.relu(self.conv_1(x))
-        x = self.relu(self.conv_2(x))
-        x = self.relu(self.conv_3(x))
-        x = self.relu(self.conv_4(x))
+        x = self.elu(self.conv_0(x))
+        # print(x.flatten()[0], x.shape)
+        x = self.elu(self.conv_1(x))
+        # print(x.flatten()[0], x.shape)
+        x = self.elu(self.conv_2(x))
+        # print(x.flatten()[0], x.shape)
+        x = self.elu(self.conv_3(x))
+        # print(x.flatten()[0], x.shape)
+        x = self.elu(self.conv_4(x))
+        # print(x.flatten()[0], x.shape)
         x = self.dropout(x)
 
+        x = x.permute(0, 3, 2, 1)
+        # print(x, x.shape)
         x = x.flatten(start_dim=1)
-        x = self.relu(self.fc0(x))
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
+        # print(x)
+        # print(x)
+        x = self.elu(self.fc0(x))
+        # print(x)
+        x = self.elu(self.fc1(x))
+        # print(x)
+        x = self.elu(self.fc2(x))
+        # print(x)
         x = self.fc3(x)
+        # print(x)
 
         return x
 
@@ -85,11 +106,12 @@ if __name__ == "__main__":
     device = args.device
     transforms_composed = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        transforms.Lambda(lambda x: x.transpose(1, 2))
     ])
     train = DataLoader(DrivingDataset('../data/sullychen/driving_dataset/', 'data.txt', True, transforms_composed), batch_size=BATCH_SIZE)
     val = DataLoader(DrivingDataset('../data/sullychen/driving_dataset/', 'data.txt', False, transforms_composed), batch_size=BATCH_SIZE)
-    model = Dave2(pretrained=False).to(device)
+    model = Dave2(pretrained=True).to(device)
     if args.validation:
         model.load_state_dict(torch.load('dave_{}.pth'.format(args.after))['model'])
         model.eval()
@@ -101,13 +123,20 @@ if __name__ == "__main__":
     percentage = len(train) // 100
     for epoch in range(epochs):
         total_loss = 0
+        model.eval()
         with torch.no_grad():
             for i, (x, y) in enumerate(val):
+                print(i / len(val))
+                # print(x.shape)
+                # print(i, x, y)
                 x = x.to(device)
                 y = y.to(device)
                 model_output = model(x)
+                # print(model_output * 180 / numpy.pi)
+                # exit()
                 total_loss += float(criterion(model_output, y))
         print("Report loss", total_loss)
+        model.train()
         if not args.validation:
             for i, (x, y) in enumerate(train):
                 optimizer.zero_grad()
