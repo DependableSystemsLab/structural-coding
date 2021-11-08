1156061a5d6d66233e3b0fa5b4ce019914769704
Ali Asgari Khoshouyeh <aasgarik@cedar5.cedar.computecanada.ca>
2021-11-07 18:30:50 -0800

    Fix opencv load

--- linearcode/script.sh
+++ linearcode/script.sh
@@ -3,10 +3,10 @@
 #SBATCH --mem=8G
 #SBATCH --array 0-1959
 
-module load opencv
 export IMAGENET_ROOT=/home/aasgarik/scratch/data/imagenet
 export PYTHONPATH=$PYTHONPATH:/home/aasgarik/projects/def-karthikp/aasgarik/reasilience
 source /home/aasgarik/projects/def-karthikp/aasgarik/dnnfault/venv/bin/activate
+module load opencv
 INTERNAL_SIZE=40
 for i in $( eval echo {1..$INTERNAL_SIZE} ); do
   export INTERNAL_SLURM_ARRAY_TASK_ID=$(( SLURM_ARRAY_TASK_ID * INTERNAL_SIZE + i - 1 ))
