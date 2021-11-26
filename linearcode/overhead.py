5fdf044bb891ea6399592caadba42eaff8b10cdd
Ali Asgari <altostratous@gmail.com>
2021-11-25 22:41:38 -0800

    Fix worst case overhead

--- linearcode/overhead.py
+++ linearcode/overhead.py
@@ -18,6 +18,7 @@ from injection import convert, StructuralCodedConv2d, StructuralCodedLinear, Cli
 #     print(i, papi_high.stop_counters()[0], sep=',')
 # exit()
 from linearcode.protection import PROTECTIONS
+from storage import get_storage_filename
 
 torch.random.manual_seed(0)
 imagenet_image = torch.rand((1, 3, 299, 299))
@@ -32,40 +33,58 @@ def flops(input_image, _model):
     return papi_high.stop_counters()[0]
 
 
-for k in (1, 2, 4, 8, 16, 32):
-    with torch.no_grad():
-        for model_name, model_class in MODEL_CLASSES:
-            # if model_name not in ('shufflenet', ):
-            #     continue
-            if model_name == 'e2e':
-                image = e2e_image
-            else:
-                image = imagenet_image
-            model = model_class()
-            model = model.double()
-            image = image.double()
+for model_name, model_class in MODEL_CLASSES:
+    if model_name != 'mobilenet':
+        continue
+    correction_filename = get_storage_filename({'fig': 'correction_flops_overhead',
+                                                'model': model_name},
+                                               extension='.tex', storage='../ubcthesis/data/')
 
-            baseline_flops = flops(image, model)
-            sc_normalized_model = PROTECTIONS['before_quantization']['sc'](model, None)
-            sc_detection_model = PROTECTIONS['after_quantization']['sc'](sc_normalized_model, {'flips': 1})
-            sc_correction_model = PROTECTIONS['after_quantization']['sc'](sc_detection_model, {'flips': k})
+    detection_filename = get_storage_filename({'fig': 'detection_flops_overhead'},
+                                              extension='.tex', storage='../ubcthesis/data/')
+    with open(detection_filename, mode='w') as detection_file:
+        with open(correction_filename, mode='w') as correction_file:
+            with torch.no_grad():
 
-            sc_detection_flops = flops(image, sc_detection_model)
-            sc_normalization_flops = flops(image, sc_normalized_model)
-
-            params = sorted(list(p for p in sc_correction_model.parameters() if len(p.shape) > 2), key=lambda p: p.flatten().shape[0] / p.shape[0], reverse=True)
-
-            for i in range(k):
-                if len(params[0].shape) > 2:
-                    params[0][i * (n + k) % params[0].shape[0]] = 1e16
+                if model_name == 'e2e':
+                    image = e2e_image
                 else:
-                    params[0][:, i * (n + k) % params[0].shape[0]] = 1e16
+                    image = imagenet_image
+                model = model_class()
+                model = model.double()
+                image = image.double()
 
-            sc_correction_flops = flops(image, sc_correction_model)
+                baseline_flops = flops(image, model)
+                sc_normalized_model = PROTECTIONS['before_quantization']['sc'](model, None)
+                sc_detection_model = PROTECTIONS['after_quantization']['sc'](sc_normalized_model, {'flips': 1})
+                sc_detection_flops = flops(image, sc_detection_model)
 
-            print(model_name,
-                  # 100 * (sc_normalization_flops / baseline_flops - 1),
-                  100 * (sc_detection_flops / baseline_flops - 1),
-                  100 * (sc_correction_flops / baseline_flops - 1))
+                print(model_name,
+                      100 * (sc_detection_flops / baseline_flops - 1), file=detection_file)
+                detection_file.flush()
 
-    print()
+                for k in (1, 2, 4, 8, 16, 32):
+
+                    sc_correction_model = PROTECTIONS['after_quantization']['sc'](sc_normalized_model, {'flips': 32})
+
+                    # sc_normalization_flops = flops(image, sc_normalized_model)
+
+                    params = sorted(list(p for p in sc_correction_model.parameters() if len(p.shape) > 1),
+                                    key=lambda p: p.flatten().shape[0] / p.shape[0], reverse=True)
+
+                    p_iterator = iter(params)
+                    parameter = next(p_iterator)
+                    offset = 0
+                    for i in range(k):
+                        while (i - offset) * (n + k) > parameter.shape[0]:
+                            parameter = next(p_iterator)
+                            offset = i
+                        parameter[(i - offset) * (n + k) % parameter.shape[0]] = 1e16
+
+                    sc_correction_flops = flops(image, sc_correction_model)
+
+                    print(k,
+                          100 * (sc_correction_flops / baseline_flops - 1), file=correction_file)
+                    correction_file.flush()
+
+            print()
