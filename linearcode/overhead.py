f71f47af6e49baf5a1c103bfe36b8bc442b67b8d
Ali Asgari <altostratous@gmail.com>
2021-12-05 09:53:09 -0800

    Change overhead to time

--- linearcode/overhead.py
+++ linearcode/overhead.py
@@ -26,12 +26,20 @@ e2e_image = torch.rand((1, 3, 200, 66))
 n = 256
 
 
-def flops(input_image, _model):
+def count_flops(input_image, _model):
     papi_high.start_counters([papi_events.PAPI_DP_OPS])
     # papi_high.start_counters([papi_events.PAPI_SP_OPS])
     _model.forward(input_image)
     return papi_high.stop_counters()[0]
 
+
+def measure_time(input_image, _model):
+    start = time()
+    _model.forward(input_image)
+    return time() - start
+
+
+measure = measure_time
 
 detection_filename = get_storage_filename({'fig': 'detection_flops_overhead'},
                                           extension='.tex', storage='../ubcthesis/data/')
@@ -41,7 +49,7 @@ memory_filename = get_storage_filename({'fig': 'memory_overhead'},
 
 with open(memory_filename, mode='w') as memory_file:
     for model_name, model_class in MODEL_CLASSES:
-        model = model_class().double()
+        model = model_class()
         sc_normalized_model = PROTECTIONS['before_quantization']['sc'](model, None)
         sc_correction_model = PROTECTIONS['after_quantization']['sc'](sc_normalized_model, {'flips': 32})
         correction_size = sum(p.nelement() for p in sc_correction_model.parameters())
@@ -50,8 +58,8 @@ with open(memory_filename, mode='w') as memory_file:
 
 with open(detection_filename, mode='w') as detection_file:
     for model_name, model_class in MODEL_CLASSES:
-        if model_name != 'shufflenet':
-            continue
+        # if model_name != 'shufflenet':
+        #     continue
         correction_filename = get_storage_filename({'fig': 'correction_flops_overhead',
                                                     'model': model_name},
                                                    extension='.tex', storage='../ubcthesis/data/')
@@ -65,14 +73,14 @@ with open(detection_filename, mode='w') as detection_file:
                 now = time()
                 model = model_class(pretrained=True)
                 print(time() - now)
-                model = model.double()
-                image = image.double()
+                model = model
+                image = image
 
-                baseline_flops = flops(image, model)
+                baseline_flops = measure(image, model)
                 sc_normalized_model = PROTECTIONS['before_quantization']['sc'](model, None)
                 # sc_normalized_model = model
                 sc_detection_model = PROTECTIONS['after_quantization']['sc'](sc_normalized_model, {'flips': 1})
-                sc_detection_flops = flops(image, sc_detection_model)
+                sc_detection_flops = measure(image, sc_detection_model)
 
                 print(model_name,
                       100 * (sc_detection_flops / baseline_flops - 1), file=detection_file)
@@ -97,7 +105,7 @@ with open(detection_filename, mode='w') as detection_file:
                         parameter[(i - offset) * (n + k) % parameter.shape[0]] = 1e16
 
                     now = time()
-                    sc_correction_flops = flops(image, sc_correction_model)
+                    sc_correction_flops = measure(image, sc_correction_model)
                     print(time() - now)
 
                     print(k,
