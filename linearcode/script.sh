2c7cd856c98d068df45e9a3c130934ca0b9d9f6e
Ali Asgari <altostratous@gmail.com>
2022-03-10 13:11:28 -0800

    Add singularity deployment

--- linearcode/script.sh
+++ linearcode/script.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH --time=18:00:00
 #SBATCH --mem=16G
-#SBATCH --array 0-60
+#SBATCH --array 0-0
 
 export IMAGENET_ROOT=/home/aasgarik/scratch/data/imagenet
 export SHARD=quantized
-export PYTHONPATH=$PYTHONPATH:/home/aasgarik/projects/def-karthikp/aasgarik/reasilience
-source /home/aasgarik/projects/def-karthikp/aasgarik/dnnfault/venv/bin/activate
+
 INTERNAL_SIZE=40
 for i in $( eval echo {1..$INTERNAL_SIZE} ); do
   export INTERNAL_SLURM_ARRAY_TASK_ID=$(( SLURM_ARRAY_TASK_ID * INTERNAL_SIZE + i - 1 ))
   echo $INTERNAL_SLURM_ARRAY_TASK_ID
-  timeout 5m python map.py
+  timeout 5m singularity exec --no-home -B ..:/code -B /scratch -B /localscratch ../sc.sif python map.py
 done
