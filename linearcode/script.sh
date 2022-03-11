c0dd6b2346df7a37be72e32b27822bf6b863200d
Ali Asgari <altostratous@gmail.com>
2022-03-10 16:01:00 -0800

    Fix singularity home directory

--- linearcode/script.sh
+++ linearcode/script.sh
@@ -4,6 +4,7 @@
 #SBATCH --array 0-0
 
 module load singularity
+mkdir -p ../shome
 
 export IMAGENET_ROOT=/home/aasgarik/scratch/data/imagenet
 export SHARD=quantized
@@ -12,5 +13,5 @@ INTERNAL_SIZE=40
 for i in $( eval echo {1..$INTERNAL_SIZE} ); do
   export INTERNAL_SLURM_ARRAY_TASK_ID=$(( SLURM_ARRAY_TASK_ID * INTERNAL_SIZE + i - 1 ))
   echo $INTERNAL_SLURM_ARRAY_TASK_ID
-  timeout 5m singularity exec --no-home --pwd /code/linearcode -B ..:/code -B /scratch -B /localscratch ../sc.sif python map.py
+  timeout 5m singularity exec --no-home --pwd /code/linearcode -B ..:/code -B ../shome:/home/aasgarik -B /scratch -B /localscratch ../sc.sif python map.py
 done
