03f10d59036d7683c94710ed4d590223ac2a785c
Ali Asgari <altostratous@gmail.com>
2022-04-07 16:24:31 -0700

    Add imagenet_ds_128

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 BATCH_SIZE = int(os.environ.get('BATCH_SIZE', '16'))
 INJECTIONS_RANGE = [int(i) for i in os.environ.get('INJECTIONS_RANGE', '0-400-1').split('-')]
 IMAGENET1K_PATH = os.environ.get('IMAGENET1K_PATH', '../data/random20classes_FI')
-IMAGENET_ROOT = os.environ.get('IMAGENET_ROOT', '../data/imagenet/')
+IMAGENET_ROOT = os.environ.get('IMAGENET_ROOT', '../data/imagenet_ds_128/')
 IMAGENET_PATH = os.environ.get('IMAGENET_PATH', os.path.join(IMAGENET_ROOT, 'ILSVRC/Data/CLS-LOC'))
 IMAGENET20P_SAMPLER_PATH = os.environ.get('IMAGENET20P_SAMPLER_PATH', '../imagenet20sampler.pkl')
 COMMA_MODEL_ROOT = os.environ.get('COMMA_MODEL_ROOT', 'comma')
