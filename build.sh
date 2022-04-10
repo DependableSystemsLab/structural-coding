b8dbe28102e62f0abafaebfb7a868c3162a8b372
Ali Asgari <altostratous@gmail.com>
2022-04-10 14:58:04 -0700

    Build on Ali's laptop

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ docker build -t dsn2022paper165/sc .
 docker push dsn2022paper165/sc
 singularity build sc.sif docker://dsn2022paper165/sc:latest
 scp sc.sif aasgarik@cedar.computecanada.ca:/home/aasgarik/projects/def-karthikp/aasgarik/reasilience/sc.sif
+scp linearcode/script.sh aasgarik@cedar.computecanada.ca:/home/aasgarik/projects/def-karthikp/aasgarik/reasilience/linearcode/script.sh
