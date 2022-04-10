0e197fd0ef77e2a4a59137fe146d70d66c025290
Ali Asgari <altostratous@gmail.com>
2022-04-10 15:10:32 -0700

    Fix script results dir

--- linearcode/script.sh
+++ linearcode/script.sh
@@ -5,6 +5,7 @@
 
 module load singularity
 mkdir -p ../shome
+mkdir -p results
 
 export SHARD=resnet50coverage
 
@@ -12,5 +13,5 @@ INTERNAL_SIZE=40
 for i in $( eval echo {1..$INTERNAL_SIZE} ); do
   export INTERNAL_SLURM_ARRAY_TASK_ID=$(( SLURM_ARRAY_TASK_ID * INTERNAL_SIZE + i - 1 ))
   echo $INTERNAL_SLURM_ARRAY_TASK_ID
-  timeout 5m singularity exec --no-home --pwd /code/linearcode -B ../shome:/home/aasgarik ../sc.sif python map.py
+  timeout 5m singularity exec --no-home --pwd /code/linearcode -B ../shome:/home/aasgarik -B results/:/code/linearcode/results/ ../sc.sif python map.py
 done
